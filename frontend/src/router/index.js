022feb541d5b634bceba7d3aa6d34ed3275dffb9
Akber - linux <akberjag@gmail.com>
2024-10-15 01:35:04 +0100

    🚦 feat(router): Add dynamic route for PortfolioView in the router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PortfolioView from '@/views/PortfolioView.vue'
 
-const routes = []
+const routes = [{ path: '/:section?', component: PortfolioView }]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
