189a3c31326f944b84998123fe999e0516d65bcf
Akber - linux <akberjag@gmail.com>
2024-10-20 11:02:38 +0100

    ✨ feat: Add About and Projects sections as examples

---
+++ frontend/src/components/sections/AboutSection.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true
+    }
+});
+</script>
+<template>
+    <div class="max-w-2xl text-center px-4">
+        <h2 class="text-2xl md:text-3xl font-bold text-white mb-4 md:mb-6">{{ data.title }}</h2>
+        <p class="text-sm md:text-lg text-white">
+            {{ data.description }}
+        </p>
+    </div>
+</template>
